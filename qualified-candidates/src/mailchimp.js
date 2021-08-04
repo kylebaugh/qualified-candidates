d695710118ac6f5e766f3951eab6a9b8c39cf2da
kylebaugh <kylebaugh1@gmail.com>
2021-08-04 13:57:34 -0600

    responsive design

---
+++ qualified-candidates/src/mailchimp.js
@@ -0,0 +1,47 @@
+{/* <!-- Begin Mailchimp Signup Form --> */}
+
+// <div id="mc_embed_signup">
+
+// <form action="https://yourdigitalcanvas.us6.list-manage.com/subscribe/post?u=7dbf922c554c0aedce97ea470&amp;id=601fc6f61d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+//   <div id="mc_embed_signup_scroll">
+//     <p class='contactRightTop'>Contact Request</p>
+  
+        
+//     <div class="mc-field-group">
+//       <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span></label>
+//       <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL"/>
+//     </div>
+            
+//     <div class="mc-field-group">
+//       <label for="mce-FNAME">First Name </label>
+//       <input type="text" value="" name="FNAME" class="" id="mce-FNAME"/>
+//     </div>
+        
+//     <div class="mc-field-group">
+//       <label for="mce-LNAME">Last Name </label>
+//       <input type="text" value="" name="LNAME" class="" id="mce-LNAME"/>
+//     </div>
+            
+//     <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+    
+//     <div id="mce-responses" class="clear">
+//       <div class="response" id="mce-error-response" style={{display:'none'}}></div>
+//       <div class="response" id="mce-success-response" style={{display:'none'}}></div>
+//     </div> 
+//     <div class='asdf'>
+//       <input type="submit" value="Subscribe" 
+//         name="subscribe" id="mc-embedded-subscribe" 
+//         class="contactButton"/>
+//     </div>
+    
+//     <div class="clear">
+//     </div>
+
+//     {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
+//     <div style={{position:'absolute', left:'-5000px'}} aria-hidden="true"><input type="text" name="b_7dbf922c554c0aedce97ea470_601fc6f61d" tabindex="-1" value=""/></div>
+    
+//   </div>
+// </form>
+// </div>
+
+{/* <!--End mc_embed_signup--> */}
