96c40b4e03d683e195a022a1eb20fd45caaf27f0
kylebaugh <kylebaugh1@gmail.com>
2021-09-08 13:21:22 -0600

    added emailjs form to replace Mailchimp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import './responsive.css'
 import { useState } from 'react';
-// import SmoothScroll from 'smooth-scroll';
+import ContactUs from './emailForm'
 
 // Logos
 import qualifiedCandidates from './assets/logos/qualified-candidates-1.png'
@@ -35,6 +35,8 @@ import clock2 from './assets/icons/asset-1.png'
 
 //Contact Form
 import emailIcon from './assets/icons/036-email.png'
+import { InlineWidget } from 'react-calendly';
+<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js"></script>
 
 
 // Smooth Scroll Functionality 
@@ -45,23 +47,6 @@ window.onload=function(){
   parent = document.getElementsByClassName(".headerMenu")
   console.log(`${parent}`)
 }
-
-// parent.addEventListener("click", function(e){
-//   const child1 = e.target.matches(".menuItem1, .menuItem1 *")
-//   const child2 = e.target.matches(".menuItem2, .menuItem2 *")
-  
-//   if (child1) {
-//     document.querySelector('.theProblem').scrollIntoView({
-//       behavior: 'smooth'
-//     })
-//   }
-  
-//   if (child2){
-//     document.querySelector('.howItWorks').scrollIntoView({
-//       behavior: 'smooth'
-//     })
-//   }
-// })
 
 function scrollToProblem(){
     document.querySelector('.theProblem').scrollIntoView({
@@ -75,18 +60,15 @@ function scrollToWorks(){
   })
 }
 
-function calendlyLink(){
-  console.log('clicky')
-}
-
-
 // BEGIN APP
 
 function App() {
-  const [name, setName] = useState('')
-  const [emailAddress, setEmailAddress] = useState('')
-  const [messageSubmission, setMessageSubmission] = useState('')
+  const [showCalendly, setShowCalendly] = useState(false)
   
+  function calendlyLink(){
+    setShowCalendly(!showCalendly)
+    console.log('clicky')
+  }
 
   return (
     <div className="App">
@@ -111,8 +93,16 @@ function App() {
           // onClick={() => clicker()} 
           >Contact Us</button>
         </section>
+
+
+
       </div>
 
+{/* Calendly Widget */}
+    {showCalendly && <div className='calendly'>
+      <InlineWidget className="widget1" url="https://calendly.com/find-qualified-canditates/experiment1et"/>
+
+    </div>}
 
 {/* Hero Section */}
 
@@ -358,70 +348,9 @@ function App() {
         </div>
 
         <div className='contactRight'>
-          {/* <section className='contactRightTop'>
-            <input value={name} placeholder='Name' className='contactName' onChange={(e) => setName(e.target.value)}></input>
-            <input value={emailAddress} placeholder='Email' className='contactEmail' onChange={(e) => setEmailAddress(e.target.value)}></input>
-          </section>
-          <br></br>
-          <br></br>
-          <textarea value={messageSubmission} type='text' placeholder='Message' className='contactMessage' onChange={(e) => setMessageSubmission(e.target.value)}></textarea>
-          <br></br>
-          <br></br>
-          <button className='contactButton'>Submit</button> */}
-
-
-{/* <!-- Begin Mailchimp Signup Form --> */}
-
-  <div id="mc_embed_signup">
-
-    <form action="https://yourdigitalcanvas.us6.list-manage.com/subscribe/post?u=7dbf922c554c0aedce97ea470&amp;id=601fc6f61d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-      <div id="mc_embed_signup_scroll">
-        <h2>Contact Request</h2>
-      
-            
-        {/* <div class="indicates-required"><span class="asterisk">*</span> indicates required</div> */}
-        <div class="mc-field-group">
-          <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span></label>
-          <input type="email" value={emailAddress} name="EMAIL" class="required email" id="mce-EMAIL"
-          onChange={(e) => setEmailAddress(e.target.value)}
-          />
-        </div>
-                
-        <div class="mc-field-group">
-          <label for="mce-FNAME">First Name </label>
-          <input type="text" value={name} name="FNAME" class="" id="mce-FNAME"
-          onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-            
-        <div class="mc-field-group">
-          <label for="mce-LNAME">Last Name </label>
-          <input type="text" value={messageSubmission} name="LNAME" class="" id="mce-LNAME"
-          onChange={(e) => setMessageSubmission(e.target.value)}
-          />
-        </div>
-                
-        
-        <div id="mce-responses" class="clear">
-          <div class="response" id="mce-error-response" style={{display:'none'}}></div>
-          <div class="response" id="mce-success-response" style={{display:'none'}}></div>
-        </div> 
-
-        {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
-        <div style={{position:'absolute', left:'-5000px'}} aria-hidden="true"><input type="text" name="b_7dbf922c554c0aedce97ea470_601fc6f61d" tabindex="-1" value=""/></div>
-        <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"/></div>
-        
-      </div>
-    </form>
-  </div>
-
-{/* <!--End mc_embed_signup--> */}
-
-
-
-
-
           
+          <ContactUs/>
+
         </div>
       </div>
 
---
+++ src/emailForm.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import emailjs from 'emailjs-com';
+import './App.css'
+// import './ContactUs.css';
+
+const {EMAIL_JS_API} = process.env
+
+function ContactUs() {
+
+    function sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('gmail', 'findtqc-submission', e.target, {EMAIL_JS_API})
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset()
+      }
+
+    return (
+        <div className='contactRight'>
+            <form className='contactFormSection' onSubmit={sendEmail}>
+                
+                <div className='contactRightTop'>
+                    <div className='contactNameDiv'>
+                        <input className='emailName' placeholder="Name" name='from_name'/>
+                    </div>
+                    <div className='contactEmailDiv'>
+                        <input className='emailAddress' placeholder='Email Address' name='email'/>
+                    </div>
+                </div>
+                
+                <div className='contactRightBottom'>
+                    <div className='contactMessageDiv'>
+                        <textArea className='emailMessage' placeholder='Your Message' name='message'/>
+                    </div>
+                    <div className='contactButtonDiv'>
+                        <input type='submit' className='emailSubmit' value='Send Message'/>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default ContactUs
